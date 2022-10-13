87ec997ca0ddd465b3167074544703e6e078361b
ario@riz.ky <rizkyario@me.com>
2022-10-13 22:07:15 +0200

    Updates

---
+++ precache-manifest.40c8245895a4a4474de9fa0f9e743cf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91f73ad9f5e5ed01bde0e93d4db3bd3",
+    "url": "/waveportal/index.html"
+  },
+  {
+    "revision": "c5ce5194ebf9c21f5f3d",
+    "url": "/waveportal/static/css/main.0e632e19.chunk.css"
+  },
+  {
+    "revision": "f1d3d52cb70c6a80652c",
+    "url": "/waveportal/static/js/2.c526ec28.chunk.js"
+  },
+  {
+    "revision": "c5ce5194ebf9c21f5f3d",
+    "url": "/waveportal/static/js/main.5bc2715b.chunk.js"
+  },
+  {
+    "revision": "b6d62a30241270d78de1",
+    "url": "/waveportal/static/js/runtime-main.21e87cc8.js"
+  }
+]);
