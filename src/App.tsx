f1561c75d9a144b68bfbbbc30a5923af8c06a1cc
Rizky Ario <rizkyario@me.com>
2022-10-15 14:17:56 +0200

    Call smart contract

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+import { ethers } from "ethers";
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import abi from "./utils/WavePortal.json";
 
 const getEthereumObject = () => window.ethereum;
 
@@ -38,6 +41,8 @@ const findMetaMaskAccount = async () => {
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const contractAddress = "0x5bce133B6C4217E990DED22C00688F03Bb5540A5";
+  const contractABI = abi.abi;
 
   const connectWallet = async () => {
     try {
@@ -58,15 +63,48 @@ const App = () => {
     }
   };
 
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        /*
+        * Execute the actual wave from your smart contract
+        */
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   /*
    * This runs our function when the page loads.
    * More technically, when the App component "mounts".
    */
-  useEffect(async () => {
+  useEffect(() => {
+    (async () => {
     const account = await findMetaMaskAccount();
     if (account !== null) {
       setCurrentAccount(account);
     }
+  })();
   }, []);
 
   return (
@@ -80,7 +118,7 @@ const App = () => {
           Hello I am Riz? Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={null}>
+        <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
 
